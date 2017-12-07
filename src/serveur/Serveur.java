8a35f8d8de61f738486d80e3a7844daa70d75e75
jonatjano <jonathan.selle@laposte.net>
2017-12-07 08:41:11 +0100

    modification de la classe serveur

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -1,15 +1,16 @@
-
-
-ServerSocket ss = new ServerSocket(...);
-while (true) // on boucle
+public class Serveur
 {
-	// attendre patiemment un client
-	Socket s  = ss.accept();
-	// créer un GerantDeClient pour traiter ce nouveau client
-	GerantDeClient gdc = new GerantDeClient(s);
-	// mettre ce gérant de client dans une Thread
-	Thread tgdc =  new Thread(gdc);
-	// lancer la thread qui gérera ce client
-	tgdc.start();
-	// ... et on recommence...
+	ServerSocket ss = new ServerSocket();
+	while (true) // on boucle
+	{
+		// attendre patiemment un client
+		Socket s  = ss.accept();
+		// créer un GerantDeClient pour traiter ce nouveau client
+		GerantDeClient gdc = new GerantDeClient(s);
+		// mettre ce gérant de client dans une Thread
+		Thread tgdc =  new Thread(gdc);
+		// lancer la thread qui gérera ce client
+		tgdc.start();
+		// ... et on recommence...
+	}
 }
